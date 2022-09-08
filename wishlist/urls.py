a82d2b1fe0d1f8e51b8e2f7e74c28ed2ba777cc5
audreyzefanya <audreyfanyap@gmail.com>
2022-09-08 14:38:37 +0700

    lab 1

---
+++ wishlist/urls.py
@@ -0,0 +1,10 @@
+
+
+from django.urls import path, include
+from wishlist.views import show_wishlist
+
+app_name = 'wishlist'
+
+urlpatterns = [
+    path('', show_wishlist, name='show_wishlist'),
+]
